16d8464771680ffb78873258caa78031db70b3ed
Jardier <jardier@hotmail.com>
2020-06-29 21:52:45 -0300

    Alterando icone do menu

--- app/src/main/java/com/android/atm/consultoria/MainActivity.kt
+++ app/src/main/java/com/android/atm/consultoria/MainActivity.kt
@@ -32,11 +32,11 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        //Configuração dos menus
         appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-            R.id.nav_tools, R.id.nav_share, R.id.nav_send), drawerLayout)
+
+        ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
